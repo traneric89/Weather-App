474f958da2e7545695cb3998b47a80ab4ec50809
optiX <55358375+traneric89@users.noreply.github.com>
2022-03-15 22:08:36 -0400

    retrieve and catch errors from api call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import { getWeather } from "./weather.js";
+import { weatherRequest } from "./weather.js";
 
 let inputCity = document.getElementById("city");
 let submitButton = document.getElementById("submit-btn");
+let div = document.getElementById("weather");
+let img = document.getElementById("icon");
 
 submitButton.addEventListener("click", (e) => searchCity(e));
 
 const searchCity = (e) => {
-  console.log(inputCity.value);
   e.preventDefault();
+  getWeather(inputCity.value);
 };
 
-getWeather();
+const getWeather = async (city) => {
+  try {
+    const response = await weatherRequest(city);
+    div.textContent = response.current.condition.icon;
+    img.src = response.current.condition.icon;
+    console.table(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,14 @@
-async function getWeather() {
+const weatherRequest = async (city) => {
   const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=d0a1fcd9a4b6423b92752725212711&q=London&aqi=no",
+    `http://api.weatherapi.com/v1/current.json?key=d0a1fcd9a4b6423b92752725212711&q=${city}&aqi=no`,
     { mode: "cors" }
   );
-  const weatherData = await response.json();
-  console.log(weatherData);
-}
+  if (response.status >= 400 && response.status < 600) {
+    throw new Error("Invalid City");
+  } else {
+    const weatherData = await response.json();
+    return weatherData;
+  }
+};
 
-export { getWeather };
+export { weatherRequest };
