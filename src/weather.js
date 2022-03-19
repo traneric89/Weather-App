b56be4a9aa8b6c01a76c8f34bb8eb31172e4f2f8
optiX <55358375+traneric89@users.noreply.github.com>
2022-03-18 23:50:50 -0400

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 const weatherRequest = async (city) => {
   const response = await fetch(
-    `https://crossorigin.me/http://api.weatherapi.com/v1/forecast.json?key=d0a1fcd9a4b6423b92752725212711&q=${city}&days=3&aqi=no&alerts=no`,
+    `http://api.weatherapi.com/v1/forecast.json?key=d0a1fcd9a4b6423b92752725212711&q=${city}&days=3&aqi=no&alerts=no`,
     { mode: "cors" }
   );
   if (response.status >= 400 && response.status < 600) {
